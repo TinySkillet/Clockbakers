985ec25f4cc795430ae73c0fad247f72e59ae758
Aditya <mirageaditya@gmail.com>
2025-03-28 03:48:28 +0545

    Fix uuid

--- sql/schema/006_carts.sql
+++ sql/schema/006_carts.sql
@@ -7,7 +7,7 @@ CREATE TABLE carts (
 
 INSERT INTO carts (
   ID, user_id )
-VALUES ('g30ac10e-58cc-4392-a587-0e03b2c3d480', 'f47ac10b-58cc-4372-a567-0e02b2c3d479');
+VALUES ('7f3a1e4c-92f5-4e8b-9d2a-1b3f5e7d9c0b', 'f47ac10b-58cc-4372-a567-0e02b2c3d479');
 
 -- +goose Down
 DROP TABLE carts;
